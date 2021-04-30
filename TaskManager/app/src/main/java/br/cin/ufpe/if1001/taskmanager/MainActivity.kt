ae465dc6d4fb4bbe14ffa0425190933ba4db294e
b0rba <borbalves.matheus@gmail.com>
2021-04-29 22:25:08 -0300

    feat: add notify mobile data is on when wifi is on

--- TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/MainActivity.kt
+++ TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/MainActivity.kt
@@ -1,31 +1,75 @@
 package br.cin.ufpe.if1001.taskmanager
 
-import android.app.Dialog
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import br.cin.ufpe.if1001.taskmanager.databinding.ActivityMainBinding
+import br.cin.ufpe.if1001.taskmanager.workers.WifiCheckerWorker
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val SHARED_PREFERENCE = "br.cin.ufpe.if1001.taskmanager.MainActivity"
+        const val MOBILE_DATA_OFF_BUTTON_STATE = "mobile_data_off_wifi_button_state"
+        const val MOBILE_DATA_OFF_WORKER_TAG = "mobile_data_off_wifi_worker_tag"
+        const val NOTIFICATION_CHANNEL_ID = "6969"
+        const val NOTIFICATION_CHANNEL_NAME = "Main Notification Channel"
+    }
+
+    private val workManager = WorkManager.getInstance(this)
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.blueLightFilter.cardText.text = "Filtro de Luz Azul"
-        binding.blueLightFilter.cardText.setOnClickListener {
-            binding.blueLightFilter.buttonToggle.performClick()
-            Toast.makeText(this, "clicou xalalal", Toast.LENGTH_SHORT).show() }
+        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCE, MODE_PRIVATE)
 
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
 
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
 
-        binding.bluetoothCustomSettings.cardText.text = "Configurações boladonas de bluetooth"
-        binding.mobileDataOffWifi.cardText.text = "Desligar Dados moveis no WIFI"
-        binding.screenLocakUsb.cardText.text = "Manter tela ligada no usb"
-        binding.wifiOffHome.cardText.text = "Desligar WIFI ao sair de casa"
-        binding.wrongPasswordPicture.cardText.text = "Tirar foto do ladrão"
+        // Mobile Data Off Wifi
+        val mobileDataOffCard = binding.mobileDataOffWifi
+        mobileDataOffCard.cardText.text = getString(R.string.mobile_data_off_card_text)
+        mobileDataOffCard.buttonToggle.isChecked =
+            sharedPreferences.getBoolean(MOBILE_DATA_OFF_BUTTON_STATE, false)
+
+        mobileDataOffCard.buttonToggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                val workRequest = PeriodicWorkRequest
+                    .Builder(WifiCheckerWorker::class.java, 1, TimeUnit.HOURS)
+                    .addTag(MOBILE_DATA_OFF_WORKER_TAG)
+                    .build()
+
+                workManager.enqueue(workRequest)
+                val shEditor = sharedPreferences.edit()
+                shEditor.putString(MOBILE_DATA_OFF_WORKER_TAG, MOBILE_DATA_OFF_WORKER_TAG)
+                shEditor.putBoolean(MOBILE_DATA_OFF_BUTTON_STATE, true)
+                shEditor.commit()
+
+            } else {
+                val mobileDataOffWorkerTag =
+                    sharedPreferences.getString(MOBILE_DATA_OFF_WORKER_TAG, null)
+                mobileDataOffWorkerTag?.let { workManager.cancelAllWorkByTag(it) }
+
+                val shEditor = sharedPreferences.edit()
+                shEditor.putBoolean(MOBILE_DATA_OFF_BUTTON_STATE, false)
+                shEditor.commit()
+            }
+        }
 
     }
 }
---
+++ TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/workers/WifiCheckerWorker.kt
@@ -0,0 +1,45 @@
+package br.cin.ufpe.if1001.taskmanager.workers
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import br.cin.ufpe.if1001.taskmanager.MainActivity
+import br.cin.ufpe.if1001.taskmanager.R
+
+class WifiCheckerWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
+    override fun doWork(): Result {
+        val context = super.getApplicationContext()
+
+        val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if (!wifiManager.isWifiEnabled)
+            return Result.success()
+
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+        val cmClass = Class.forName(connectivityManager.javaClass.name)
+        val method = cmClass.getDeclaredMethod("getMobileDataEnabled")
+        method.isAccessible = true
+        val mobileDataEnabled = method.invoke(connectivityManager) as Boolean
+        if (!mobileDataEnabled)
+            return Result.success()
+
+        val builder = NotificationCompat.Builder(context, MainActivity.NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(context.getString(R.string.mobile_data_off_notification_title))
+            .setContentText(context.getString(R.string.mobile_data_off_notification_text))
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(context.getString(R.string.mobile_data_off_notification_text))
+            )
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(69, builder.build())
+        }
+
+        return Result.success()
+    }
+}
