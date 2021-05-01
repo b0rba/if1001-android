3f033dad1f83b8eb8872fd03b945caf2267e3dd5
b0rba <borbalves.matheus@gmail.com>
2021-05-01 11:15:38 -0300

    feat: add splash screen and fix colors

--- TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/workers/WifiCheckerWorker.kt
+++ TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/workers/WifiCheckerWorker.kt
@@ -1,11 +1,9 @@
 package br.cin.ufpe.if1001.taskmanager.workers
 
 import android.content.Context
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import br.cin.ufpe.if1001.taskmanager.MainActivity
+import br.cin.ufpe.if1001.taskmanager.activities.MainActivity
 import br.cin.ufpe.if1001.taskmanager.R
 import br.cin.ufpe.if1001.taskmanager.utils.GeneralServices
 import br.cin.ufpe.if1001.taskmanager.utils.NotificationUtil
