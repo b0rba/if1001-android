d4f48045d67d8a532f485387794ba8bc948111de
b0rba <borbalves.matheus@gmail.com>
2021-05-03 09:20:20 -0300

    style: add comment on not used class

--- TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/utils/CallbackUtil.kt
+++ TaskManager/app/src/main/java/br/cin/ufpe/if1001/taskmanager/utils/CallbackUtil.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
+//not used on new implementation but i'm proud of this logic, so i'm leaving this in.
 class CallbackUtil {
     companion object {
         suspend fun <T> awaitCallback(block: (Callback<T>) -> Unit): T =
